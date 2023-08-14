442ace0e9139ac31191bc20c36bc4c6f34b1d9ab
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2023-08-14 18:01:30 +0300

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pages;
+
+class PagesController extends Controller
+{
+    public function create(){
+        $pages = new Pages;
+        $pages->title='Laravel Eğitimi';
+        $pages->subtitle='Laravel 9 Dersleri';
+        $pages->content='Burada Laravel Eğitimi Hakkında Bilgiler Mevcut';
+        $pages->save();
+
+        return "Veri Kaydedildi";
+    }
+
+    public function createnew(){
+        Pages::create([
+            'title'=>'Laravel Eğitimi',
+            'subtitle'=>'Laravel 9 Dersleri',
+            'content'=> 'Burada Laravel Eğitimi Hakkında Bilgiler Mevcut'
+        ]);
+
+        return "Veri Eklendi";
+    }
+
+    public function read(){
+        $pages = Pages::query()->get();
+        return view('read', compact('pages'));
+    }
+
+    public function edit($id){
+        $page = Pages::query()->find($id);
+
+        return view('edit', compact('page'));
+    }
+}
